32e53beed6dca3ef024924536161fe6af8111468
raymond_wang <ramenwangqing@gmail.com>
2019-10-26 18:51:53 -0500

    updates the regularization into the convolutional model

--- LeNet5/LeNet5_withReg.py
+++ LeNet5/LeNet5_withReg.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np 
-import functools import partial
+from functools import partial
+from tensorflow.keras import Model, Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 # set up GPU as the first GPU
 physical_devices = tf.config.experimental.list_physical_devices('GPU') 
@@ -10,20 +12,10 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 def l2_reg(coef=1e-2):
     return lambda x: tf.reduce_sum(x ** 2) * coef
 
+
 def l1_reg(coef=1e-2):
     return lambda x: tf.reduce_sum(tf.abs(x)) * coef
 
-import tensorflow as tf 
-
-# initializing the trainable variables
-k = 3
-D = 3
-N = 32
-kernels_shape = [k, k, D, N]
-glorot_uni_initializer = tf.initializers.GlorotUniform()
-
-kernels = tf.Variable(glorot_uni_initializer(kernels_shape), trainable=True, name='filters')
-bias    = tf.Variable(tf.zeros([N]), trainable=True, name='bias')
 
 # define convolutional layer as a compiled function
 @tf.function
@@ -31,7 +23,12 @@ def conv_layer(x, kernels, bias, s):
     z = tf.nn.conv2d(input=x, filters=kernels, strides=[1,s,s,1], padding='SAME')
     return tf.nn.relu(z + bias)
 
+
 class convLayer2d(tf.keras.layers.Layer):
+    '''
+    Simple convolutional neural networks
+    '''
+
     def __init__(self, num_kernel = 32, kernel_size = (3,3), stride = 1):
         super().__init__()
         self.num_kernel  = num_kernel
@@ -52,6 +49,9 @@ class convLayer2d(tf.keras.layers.Layer):
 
 
 class ConvReg2D(convLayer2d):
+    '''
+    a convolution layer calls 2d convolutional layer function and add regularizer
+    '''
 
     def __init__(self, num_kernel = 32, kernel_size = (3,3), stride = 1,\
         kernel_regularizer = l2_reg(), bias_regularizer = None):
@@ -73,43 +73,35 @@ class ConvReg2D(convLayer2d):
             self.add_loss(partial(self.bias_regularizer, self.bias))
 
 
-    
 
+if __name__ == "__main__":
 
-class LeNet5(Model):
-    def __init__(self, num_classes):
-        super(LeNet5, self).__init__()
-        self.conv1 = Conv2D(6, kernel_size=(5, 5), padding = 'same', activation = 'relu')
-        self.max_pool = MaxPooling2D(pool_size=(2, 2))
-        self.conv2 = Conv2D(16, kernel_size=(5,5), activation = 'relu')
-        self.flatten = Flatten()
-        self.dense1 = Dense(120, activation='relu')
-        self.dense2 = Dense(84, activation='relu')
-        self.dense3 = Dense(num_classes, activation='softmax')
+    model = Sequential()
+    model.add(ConvReg2D(num_kernel=6, kernel_size=(5,5), stride=1, kernel_regularizer=l2_reg()))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(ConvReg2D(num_kernel=16, kernel_size=(5,5), stride=1, kernel_regularizer=l2_reg()))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(120, activation='relu'))
+    model.add(Dense(84, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
 
-    def call(self, x):
-        x = self.conv1(x)
-        x = self.max_pool(x)
-        x = self.conv2(x)
-        x = self.max_pool(x)
-        x = self.flatten(x)
-        x = self.dense1(x)
-        x = self.dense2(x)
-        x = self.dense3(x)
-        return x
+    # initializing the trainable variables
 
-model = LeNet5(num_classes=10)
-model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', \
-    metrics=['accuracy'])
-callbacks = [
-    tf.keras.callbacks.EarlyStopping(patience=3, monitor='val_loss'),
-    tf.keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=1)
-]
+    # kernels = tf.Variable(glorot_uni_initializer(kernels_shape), trainable=True, name='filters')
+    # bias    = tf.Variable(tf.zeros([N]), trainable=True, name='bias')
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train/255.0, x_test/255.0
-x_train, x_test = x_train[...,tf.newaxis], x_test[...,tf.newaxis]
-model.fit(x_train, y_train, batch_size=24, epochs=80,\
-     validation_data=(x_test, y_test), callbacks = callbacks)
+    model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', \
+        metrics=['accuracy'])
+    callbacks = [
+        tf.keras.callbacks.EarlyStopping(patience=3, monitor='val_loss'),
+        tf.keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=1)
+    ]
+
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train),(x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train/255.0, x_test/255.0
+    x_train, x_test = x_train[...,tf.newaxis], x_test[...,tf.newaxis]
+    model.fit(x_train, y_train, batch_size=24, epochs=80,\
+        validation_data=(x_test, y_test), callbacks = callbacks)
 
